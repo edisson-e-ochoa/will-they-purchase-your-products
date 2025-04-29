742000304801070fe7434ed0d24d5aebeb01a9a4
Edisson Ochoa <edisson.e.ochoa@gmail.com>
2025-04-28 23:09:10 -0500

    Add files via upload

---
+++ my_colors.py
@@ -0,0 +1,10 @@
+my_colors = {
+    'blue':'#024CAA',
+    'orange':'#EC8305',
+    'pink':'#F2AFEF',
+    'dark_blue':'#091057',
+    'light_purple':'#C499F3',
+    'purple':'#7360DF',
+    'light_green':'#71BBB2',
+    'dark_green':'#497D74'
+}
